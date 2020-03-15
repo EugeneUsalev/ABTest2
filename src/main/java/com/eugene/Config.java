034f23a9cfc07b83068a3a681a106dd45355ca8d
EugeneUsalev <49619404+EugeneUsalev@users.noreply.github.com>
2020-03-15 17:15:09 +0400

    Update Config.java

--- src/main/java/com/eugene/Config.java
+++ src/main/java/com/eugene/Config.java
@@ -2,8 +2,5 @@ package com.eugene;
 
 public class Config {
     public static final String START_URL = "https://yandex.ru/";
-    public static final String IN_DATA_PATH = "in.txt";
-    public static final String OUT_PATH = "out.txt";
-    public static final String ERROR_PATH = "error.txt";
     public static final String SCREENSHOTS_DIR_PATH = "screen/";
 }
